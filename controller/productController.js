6ee5bbfe6bd39f3321500873f0a6d1c5ff0c45b3
GuillePujals <guille_pujals@yahoo.com.ar>
2021-04-24 18:18:26 -0300

    Guille - creación de formulario de edición de productos

--- controller/productController.js
+++ controller/productController.js
@@ -1,3 +1,6 @@
+//requerimos el array de casas
+let casas = require ('../data/datosProductos')
+
 let productController = {
 
     showDetalle: (req, res) => {
@@ -6,6 +9,26 @@ let productController = {
 
     showCarrito: (req, res) => {
         res.render('products/productCart')
+    },
+
+    showEdit: (req, res) => {
+        //console.log('ESTOY ENTRANDO AL METODO EDIT:')
+
+        let product = casas.find(function (value) {
+
+            return value.id === req.params.id
+        })
+
+        //console.log(product)
+        if (product) {
+            res.render('products/editProduct', { product });
+        } else {
+            res.render('error404');
+        }
+    },
+
+    showList: (req, res) => {
+        res.render ('products/listProducts')
     }
     
 }
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,5 +4,7 @@ const controladorproduct = require('../controller/productController');
 
 router.get('/productDetail/', controladorproduct.showDetalle);
 router.get('/productCart/', controladorproduct.showCarrito);
+router.get('/products/edit:id', controladorproduct.showEdit);
+router.get('/products/listado', controladorproduct.showList);
 
 module.exports = router;
